79a6113dfd53c6ce3a56aa759d9fd09aca369bfc
mariamf1985 <mariamf1985@gmail.com>
2024-03-15 09:16:12 +0100

    correct name for cliente factory, seeder and model

--- backend/database/seeders/ClienteSeeder.php
+++ backend/database/seeders/ClienteSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ClientesSeeder extends Seeder
+class ClienteSeeder extends Seeder
 {
     /**
      * Run the database seeds.
