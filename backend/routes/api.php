a744931ebeb086b46d5dcfd1a7fe65d85f307dbc
mariamf1985 <mariamf1985@gmail.com>
2024-03-15 18:13:01 +0100

    fill routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,32 +8,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('clientes')->group(function () {
     Route::get('/', [ClienteController::class, 'index']);
-    Route::post('/', [ClienteController::class, 'store']);
-    Route::get('/{id}', [ClienteController::class, 'show']);
-    Route::put('/{id}', [ClienteController::class, 'update']);
-    Route::delete('/{id}', [ClienteController::class, 'destroy']);
+    Route::post('/cliente', [ClienteController::class, 'store']);
+    Route::get('/cliente/{id}', [ClienteController::class, 'show']);
+    Route::put('/cliente/{id}', [ClienteController::class, 'update']);
+    Route::delete('/cliente/{id}', [ClienteController::class, 'destroy']);
 });
 
 Route::prefix('coches')->group(function () {
     Route::get('/', [CocheController::class, 'index']);
-    Route::post('/', [CocheController::class, 'store']);
-    Route::get('/{id}', [CocheController::class, 'show']);
-    Route::put('/{id}', [CocheController::class, 'update']);
-    Route::delete('/{id}', [CocheController::class, 'destroy']);
+    Route::post('/coche', [CocheController::class, 'store']);
+    Route::get('/coche/{id}', [CocheController::class, 'show']);
+    Route::put('/coche/{id}', [CocheController::class, 'update']);
+    Route::delete('/coche/{id}', [CocheController::class, 'destroy']);
 });
 
 Route::prefix('servicios')->group(function () {
     Route::get('/', [ServicioController::class, 'index']);
-    Route::post('/', [ServicioController::class, 'store']);
-    Route::get('/{id}', [ServicioController::class, 'show']);
-    Route::put('/{id}', [ServicioController::class, 'update']);
-    Route::delete('/{id}', [ServicioController::class, 'destroy']);
+    Route::post('/servicio', [ServicioController::class, 'store']);
+    Route::get('/servicio/{id}', [ServicioController::class, 'show']);
+    Route::put('/servicio/{id}', [ServicioController::class, 'update']);
+    Route::delete('/servicio/{id}', [ServicioController::class, 'destroy']);
 });
 
 Route::prefix('citas')->group(function () {
     Route::get('/', [CitaController::class, 'index']);
-    Route::post('/', [CitaController::class, 'store']);
-    Route::get('/{id}', [CitaController::class, 'show']);
-    Route::put('/{id}', [CitaController::class, 'update']);
-    Route::delete('/{id}', [CitaController::class, 'destroy']);
+    Route::post('/cita', [CitaController::class, 'store']);
+    Route::get('/cita/{id}', [CitaController::class, 'show']);
+    Route::put('/cita/{id}', [CitaController::class, 'update']);
+    Route::delete('/cita/{id}', [CitaController::class, 'destroy']);
 });
