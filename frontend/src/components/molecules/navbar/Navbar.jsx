88e72fdf253cbfcff3c026b594d7e4349fd16895
mariamf1985 <mariamf1985@gmail.com>
2024-03-18 15:25:02 +0100

    create front folders

---
+++ frontend/src/components/molecules/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>esta es la Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Esta es la home</div>
+  )
+}
+
+export default Home
