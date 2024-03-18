62a6221a669a7e2de5b191c216f38190ffe7389f
mariamf1985 <mariamf1985@gmail.com>
2024-03-18 21:37:02 +0100

    create router and routes

---
+++ frontend/src/configuration/Router.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from '../Pages/Landing';
+import Home from '../Pages/Home';
+import AllProducts from '../Pages/AllProducts';
+import Product from '../Pages/Product';
+import Cart from '../Pages/Cart';
+import CreateProduct from '../Pages/CreateProduct';
+
+function Router() {
+  return (
+    <BrowserRouter>
+    <Routes>
+        {/* <Route path="/" element={<Landing/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
+        <Route path="/allproducts" element={<AllProducts/>}></Route>
+        <Route path="/product" element={<Product/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
+        <Route path="/createproduct" element={<CreateProduct/>}></Route> */}
+    </Routes>
+</BrowserRouter>
+  )
+}
+
+export default Router
