cc97683e79b05e93dc1ecf5cfe76384225e9e074
mariamf1985 <mariamf1985@gmail.com>
2024-03-19 11:53:21 +0100

    add calendario file

---
+++ frontend/src/pages/Calendario.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/molecules/navbar/Navbar'
+import Footer from '../components/molecules/footer/Footer'
+
+const Calendario = () => {
+    return (
+        <>
+            <Navbar />
+
+            <div>Calendario</div>
+
+            <Footer />
+
+        </>
+    )
+}
+
+export default Calendario
