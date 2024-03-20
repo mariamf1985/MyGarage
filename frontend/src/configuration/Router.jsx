0ccce027d64394ec073d115586327ce826af8e88
mariamf1985 <mariamf1985@gmail.com>
2024-03-20 12:32:23 +0100

    create route and file clientes

--- frontend/src/configuration/Router.jsx
+++ frontend/src/configuration/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Citas from '../pages/Citas';
 import NuevaCita from '../pages/NuevaCita';
+import Clientes from '../pages/Clientes';
 
 
 function Router() {
@@ -11,8 +12,8 @@ function Router() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/citas" element={<Citas/>}></Route>
         <Route path="/nuevaCita" element={<NuevaCita/>}></Route>
-         {/*<Route path="/cart" element={<Cart/>}></Route>
-         <Route path="/createproduct" element={<CreateProduct/>}></Route>  */}
+        <Route path="/clientes" element={<Clientes/>}></Route>
+         
     </Routes>
 </BrowserRouter>
   )
